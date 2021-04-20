8cfabe5d53c05e99cc424034652731ef2039bd3a
Hyena-Feet <82405199+Hyena-Feet@users.noreply.github.com>
2021-04-20 14:13:50 -0400

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,17 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+scene.setBackgroundColor(9)
 let Dune_Runner = sprites.create(assets.image`The DuneRunner`, SpriteKind.Player)
 Dune_Runner.setStayInScreen(true)
-controller.moveSprite(Dune_Runner, 50, 70)
+Dune_Runner.ay = 2500
+Dune_Runner.ax = -500
 forever(function () {
-    Dune_Runner.setVelocity(-30, 0)
+	
 })
